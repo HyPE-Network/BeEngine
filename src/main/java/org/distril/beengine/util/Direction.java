4f5849ee01d5e02c1002e43d9ab93f8ef4e2ade1
IWareQ <dimonpo96@mail.ru>
2023-01-28 13:21:10 +0400

    Update Direction header license

--- src/main/java/org/distril/beengine/util/Direction.java
+++ src/main/java/org/distril/beengine/util/Direction.java
@@ -1,8 +1,23 @@
 package org.distril.beengine.util;
 
 /*
- * LICENSE: https://github.com/CloudburstMC/API/blob/bleeding/LICENSE
- * ORIGINAL FILE: https://github.com/CloudburstMC/API/blob/bleeding/src/main/java/org/cloudburstmc/api/util/Direction.java
+ * Copyright (C) 2023, Distril
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/lgpl-3.0.txt>.
+ *
+ * This program includes code from Cloudburst/API, which is licensed under the GNU Lesser General Public License v3.0.
+ * A copy of the license can be found in the project or at <https://www.gnu.org/licenses/lgpl-3.0.txt>.
  */
 
 import com.google.common.collect.Iterators;
