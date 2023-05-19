9800495b10c20be5a947a20d4530e78df03cdfd7
IWareQ <dimonpo96@mail.ru>
2023-05-19 14:45:45 +0400

    Async tick chunks

--- src/main/kotlin/org/distril/beengine/world/chunk/ChunkManager.kt
+++ src/main/kotlin/org/distril/beengine/world/chunk/ChunkManager.kt
@@ -1,7 +1,8 @@
 package org.distril.beengine.world.chunk
 
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import org.distril.beengine.util.ChunkUtils
 import org.distril.beengine.world.World
 import java.util.concurrent.ConcurrentHashMap
@@ -11,52 +12,45 @@ class ChunkManager(private val world: World) {
 
 	val chunks: MutableMap<Long, Chunk> = ConcurrentHashMap()
 
+	val random = ThreadLocalRandom.current()
+
 	fun getLoadedChunk(x: Int, z: Int) = this.getLoadedChunk(ChunkUtils.encode(x, z))
 
 	fun getLoadedChunk(key: Long) = this.chunks[key]
 
 	fun getChunk(x: Int, z: Int) = this.getChunk(ChunkUtils.encode(x, z))
 
-	fun getChunk(key: Long): Chunk {
-		var chunk = this.getLoadedChunk(key)
-		if (chunk == null) {
-			chunk = this.generateChunk(key)
-		}
-
-		return chunk
-	}
+	fun getChunk(key: Long) = this.getLoadedChunk(key) ?: this.generateChunk(key)
 
 	fun generateChunk(x: Int, z: Int) = this.generateChunk(ChunkUtils.encode(x, z))
 
-	fun generateChunk(key: Long): Chunk {
+	fun generateChunk(key: Long): Chunk = synchronized(this.chunks) {
 		val loadedChunk = this.getLoadedChunk(key)
 		if (loadedChunk != null) return loadedChunk
 
 		val chunk = this.chunks.computeIfAbsent(key) {
-			Chunk(
-				ChunkUtils.decodeX(it),
-				ChunkUtils.decodeZ(it)
-			)
+			Chunk(ChunkUtils.decodeX(it), ChunkUtils.decodeZ(it))
 		}
 
-		this.world.generator.generate(ThreadLocalRandom.current(), chunk)
+		this.world.generator.generate(this.random, chunk)
 		return chunk
 	}
 
-	suspend fun tick() {
-		if (this.chunks.isNotEmpty()) {
-			val tickedChunks = flow {
+	suspend fun tick(): Collection<Deferred<Unit>> {
+		val tickedChunks = mutableListOf<Deferred<Unit>>()
+		if (chunks.isNotEmpty()) {
+			coroutineScope {
 				chunks.forEach { (key, chunk) ->
-					val canBeUnload = chunk.tick()
+					tickedChunks.add(async {
+						val canBeUnload = chunk.tick()
 
-					if (canBeUnload) unloadChunk(key)
-
-					emit(Unit)
+						if (canBeUnload) unloadChunk(key)
+					})
 				}
 			}
-
-			tickedChunks.collect()
 		}
+
+		return tickedChunks
 	}
 
 	fun unloadChunk(key: Long, save: Boolean = true, force: Boolean = false) {
